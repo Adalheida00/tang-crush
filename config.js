b16d008ac6f567fb0be95e7c2e74abcf16f8e20a
Adalheida00 <86762880+Adalheida00@users.noreply.github.com>
2021-07-01 12:51:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'babe à!',
+    introTitle: 'bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao m cứ đi nhầm đường
+    Mà sao bạn cứ đi nhầm đường
     Lạc vào tim ai lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng m đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng m đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng bạn đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích tao <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnNo: 'Không, bạn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tui <3',
+    btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Tui biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h tui qua đón bạn nhé .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
